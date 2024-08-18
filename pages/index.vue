a703cea503d61ac745c74d3f6e9e1b49748e1fcf
AntBbx <headamonium@gmail.com>
2024-08-18 20:04:37 +0200

    sveltia

--- pages/index.vue
+++ pages/index.vue
@@ -6,4 +6,28 @@ useHead({
   ],
 });
 
+const data = ref([]);
+
+const { data: result, refresh } = await useAsyncData('bloggg', () => 
+  queryContent('blog').find()
+);
+
+if (result) {
+  data.value = unref(result);
+  console.log(data.value);
+}
 </script>
+
+<template>
+ <div v-if="data.length">
+    <div v-for="article in data" :key="article._path">
+      <NuxtLink :to="article._path">
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+      </NuxtLink>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
